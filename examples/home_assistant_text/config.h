32dec870d84b0d7551f81f6962f9e7f6d9840d4a
Mark Walker <markwal@hotmail.com>
2023-02-22 13:10:37 -0800

    Fix the example

---
+++ examples/home_assistant_text/config.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID "YourSsidHere"
+#define WIFI_PASS "YourWifiPass"
+#define MQTT_HOST "homeassistant.lan"
+#define MQTT_PORT 1883
+#define MQTT_USER "YourMqttUser"
+#define MQTT_PASS "YourMqttPassword"
--- examples/home_assistant_text/home_assistant_text.ino
+++ examples/home_assistant_text/home_assistant_text.ino
@@ -1,11 +1,21 @@
 #include <home_assistant_io.h>
 #include "config.h"
 
-HomeAssistantState* textState;
+HomeAssistantState* textState = nullptr;
 
 // command callback
 void setText(const char *text) {
+	Serial.println("Got some text:");
+	if (!text) {
+		Serial.println("nullptr");
+		return;
+	}
 	Serial.println(text);
+	if (textState == nullptr) {
+		Serial.println("textState not initialized.  Skipping MQTT command.");
+		return;
+	}
+
 	textState->setValue(text);
 	HomeAssistant.sendState();
 }
@@ -18,16 +28,17 @@ void setup() {
 	HomeAssistant.setWiFi(WIFI_SSID, WIFI_PASS);
 	HomeAssistant.setServer(MQTT_HOST, MQTT_PORT);
 	HomeAssistant.setCredentials(MQTT_USER, MQTT_PASS);
-	HomeAssistant.setDeviceId("kitchenScreen");
-	HomeAssistantComponent* textComponent = HomeAssistant.addComponent("text");
-	HomeAssistantFlatState* state = new HomeAssistantFlatState("homeassistant/kitchenScreen", "text");
-	HomeAssistantState* text = textComponent->addState(state);
-	text->onCommand(setText);
+	HomeAssistant.setDeviceId("thingy");
+
+	auto textComponent = HomeAssistant.addComponent("text");
+	textState = textComponent->addState(new HomeAssistantFlatState("homeassistant/text/thingytext", "text"));
+	textState->onCommand(setText);
 
 	// connect: connects wifi, mqtt and registers all of the auto discover topics
 	HomeAssistant.connect();
+	Serial.println("setup complete.");
 }
 
 void loop() {
-
 }
+
